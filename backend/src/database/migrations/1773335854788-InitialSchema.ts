e85060cc3f67a52750747297a84ac2686e450546
ethan-goh <ethan.goh.ye@gmail.com>
2026-03-14 22:09:29 +0800

    dockerise backend, set up backend dockerfile and add backend container to docker-compose.yml

--- backend/src/database/migrations/1773335854788-InitialSchema.ts
+++ backend/src/database/migrations/1773335854788-InitialSchema.ts
@@ -4,7 +4,7 @@ export class InitialSchema1773335854788 implements MigrationInterface {
     name = 'InitialSchema1773335854788'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "cafes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(255) NOT NULL, "description" text NOT NULL, "logo" character varying, "location" character varying(255) NOT NULL, CONSTRAINT "PK_1e8e00a60bc4dd368d8d55a1d7e" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "cafes" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "name" character varying(255) NOT NULL, "description" text NOT NULL, "logo" character varying, "location" character varying(255) NOT NULL, CONSTRAINT "PK_1e8e00a60bc4dd368d8d55a1d7e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "cafe_employees" ("cafe_id" uuid NOT NULL, "employee_id" character varying(9) NOT NULL, "start_date" date NOT NULL, CONSTRAINT "PK_8e5775223fb33174c06b9d97f17" PRIMARY KEY ("cafe_id", "employee_id"))`);
         await queryRunner.query(`CREATE TYPE "public"."employees_gender_enum" AS ENUM('Male', 'Female')`);
         await queryRunner.query(`CREATE TABLE "employees" ("id" character varying(9) NOT NULL, "name" character varying(255) NOT NULL, "email_address" character varying NOT NULL, "phone_number" character varying(8) NOT NULL, "gender" "public"."employees_gender_enum" NOT NULL, CONSTRAINT "UQ_589863617aaee6817a766a23687" UNIQUE ("email_address"), CONSTRAINT "PK_b9535a98350d5b26e7eb0c26af4" PRIMARY KEY ("id"))`);
